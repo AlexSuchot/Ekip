d659db96e2462d8e6192050b1c86c0f3e33295b2
AlexSuchot <alexandre.suchot@ynov.com>
2020-01-27 19:14:14 +0100

    Adding bloc class

--- Python/script.py
+++ Python/script.py
@@ -8,6 +8,19 @@ count = 0
 block = ''
 decount = pdfReader.numPages
 
+# Le numéro du bloc :
+blockNumber = 1
+
+
+class Bloc:
+    def __init__(self, number, sha256, pagesText, contributorUUID, proofOfWork):
+        self.number = number
+        self.sha256 = sha256
+        self.pagesText = pagesText
+        self.contributorUUID = contributorUUID
+        self.proofOfWork = proofOfWork
+
+
 # On parcours toutes les pages du livre et on affiche le texte :
 for x in range(pdfReader.numPages):
     # Affiche une page :
@@ -23,7 +36,10 @@ for x in range(pdfReader.numPages):
         print(m.hexdigest())
         block = ''
         count = 0
-        decount -= 5
 
-    elif pdfReader.numPages - x <= 5:
+        newBloc = Bloc(blockNumber, m.hexdigest(), block, 'contributorUUID', 'proofOfWork')
+
+        blockNumber += blockNumber
+
+    elif count < 5:
         print(text)
