7c0162c9b3acf7624efc19aa60fabf2d84e4526a
capucine.bechemin <capucinebechemin@gmail.com>
2020-01-28 13:41:28 +0100

    Script bdd table Block

---
+++ bdd_blockchain_ekipV1.sql
@@ -0,0 +1,23 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  mar. 28 jan. 2020 à 12:23
+-- Version du serveur :  5.7.24
+-- Version de PHP :  7.2.14
+--
+-- Base de données :  `blockchain_ekip`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `block`
+--
+
+CREATE TABLE IF NOT EXISTS `block` (
+  `contributeur` varchar(255) NOT NULL,
+  `hash` varchar(255) NOT NULL,
+  `nonce` varchar(255) NOT NULL
+);
